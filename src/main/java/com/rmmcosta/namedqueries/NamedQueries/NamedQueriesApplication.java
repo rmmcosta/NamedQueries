2bdca5fdb214df3279e6337386cf98c1a8bc9f86
Ricardo Costa <ricardocosta101085@gmail.com>
2022-11-13 15:49:09 +0000

    first commit after spring initializr

---
+++ src/main/java/com/rmmcosta/namedqueries/NamedQueries/NamedQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.rmmcosta.namedqueries.NamedQueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamedQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamedQueriesApplication.class, args);
+	}
+
+}
